c4b2b4e2b00e6f43e66020c4e95c968bf2a95860
Patrick Wu <61214736+FrostleafW@users.noreply.github.com>
2023-05-19 21:07:51 -0400

    Create plugin.js

---
+++ dist/plugin.js
@@ -0,0 +1,6 @@
+exports.version = 1
+exports.description = "Simple media viewer by FrostleafW"
+exports.apiRequired = 7
+exports.repo = "FrostleafW/simple-media-viewer"
+exports.frontend_css = 'style.css'
+exports.frontend_js = 'main.js'
